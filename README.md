e2d5c5187f9e9efe1d2945571f1b77396a779143
iantheparker <iantheparker@gmail.com>
2018-10-24 23:11:03 +0900

    handle global_lm flag
    make read_csv_sampler robust to extreme sizes

--- README.md
+++ README.md
@@ -4,14 +4,11 @@ Run on floydhub workspace. Walk through in the 00_start_here.ipynb
 
 [![Run on FloydHub](https://static.floydhub.com/button/button-small.svg)](https://floydhub.com/run?template=https://github.com/frame/transfer-learning-fastai)
 
-If you want to run jobs locally, just change the sample size
-
 ```
 floyd init frame/fastai_ulmfit_test # if you haven't created job title yet
 floyd run "bash setup.sh && python train.py mytest floyd \
---sample-size 100" \
---env pytorch-1.0 \
---gpu \
+--sample-size=100 --global-lm=False" \
+--env pytorch-1.0 --gpu \
 --follow \
 --data frame/datasets/imdb_reviews_wt103/1:imdb_reviews_wt103 \
 -m "sample size 100"
--- train.py
+++ train.py
@@ -4,30 +4,6 @@ from tensorboard_cb import *
 from fastai import *
 
 
-@dataclass
-class FloydHubLogger(Callback):
-    learn: Learner
-    run_name: str
-    histogram_freq: int = 100
-    path: str = None
-
-    def _emit_floydhub_log_line(self, key, value):
-        try:
-            value = float(value)
-        except ValueError:
-            print("Floydhub reporting only support single dimension float metrics")
-        else:
-            print(f'{{"metric": "{key}", "value": {value}}}')
-
-    def on_epoch_end(self, **kwargs):
-        lm = kwargs["last_metrics"]
-        metrics_names = ["valid_loss"] + \
-            [o.__name__ for o in self.learn.metrics]
-
-        for val, name in zip(lm, metrics_names):
-            self._emit_floydhub_log_line(name, val)
-
-
 def sample_for_experiment(sample_size, dst, env, clean_run):
     if env == 'floyd':
         src = '/floyd/input/imdb_reviews_wt103/'
@@ -43,23 +19,30 @@ def sample_for_experiment(sample_size, dst, env, clean_run):
     shutil.copytree(src, dst)
     dftr_lm = read_csv_with_sample_size(
         os.path.join(dst, 'train.csv'), sample_size=sample_size)
-    dftr_lm.to_csv(os.path.join(dst, 'train_lm.csv'), index=False)
+    dftr_lm.to_csv(os.path.join(dst, 'train_lm.csv'),
+                   header=False, index=False)
 
 
 def read_csv_with_sample_size(path, sample_size, chunksize=24000):
     total_rows = get_total_length(path, chunksize=chunksize)
-    frac = sample_size / total_rows if sample_size < total_rows else 1.0
+    chunk_length = get_chunk_length(path, chunksize=chunksize)
+    # protect against zero
+    sample_size = max(sample_size, 1)
+    # protect against a fraction too small to sample from a chunk
+    frac = min(max(sample_size, chunk_length+1) / total_rows, 1.0)
     df = pd.DataFrame()
     for chunk in pd.read_csv(path, chunksize=chunksize, header=None):
         df = pd.concat([df, chunk.sample(frac=frac)])
-    return df
+    return df[:sample_size]
 
 
-def train_language_model(data_dir, env, sample_size):
-    # TODO allow sample_size to exceed spreadhseet size. it crashes when I enter 10000
+def train_language_model(data_dir, env, sample_size, global_lm, lm_encoder_name):
+
+    if sample_size < 100:
+        return None, None
+
     data_lm = TextLMDataBunch.from_csv(data_dir,
-                                       train="train_lm",
-                                       chunksize=np.ceil(sample_size/2)+1)
+                                       train="train_lm")
     print(f'Vocabulary size: {data_lm.train_ds.vocab_size}')
 
     # TODO Everything below is an attempt to mimic the hyperparaments set in this v0.7 example
@@ -67,11 +50,12 @@ def train_language_model(data_dir, env, sample_size):
     wd = 1e-7
     lr = 1e-3
     lrs = lr
+    pretrained_fnames = ('lstm_wt103',
+                         'itos_wt103') if global_lm == True else None
     learn = RNNLearner.language_model(data_lm,
                                       drop_mult=0.7,
-                                      pretrained_fnames=(
-                                          'lstm_wt103', 'itos_wt103'),
-                                      metrics=accuracy)
+                                      pretrained_fnames=pretrained_fnames,
+                                      metrics=[accuracy])
 
     if env == 'floyd':
         cbs = [TensorboardLogger(learn, f"transfer-learning-{sample_size}"),
@@ -86,20 +70,20 @@ def train_language_model(data_dir, env, sample_size):
     learn.unfreeze()
     learn.lr_find(start_lr=lrs / 10, end_lr=lrs * 10)
     learn.fit(lr=slice(lrs, 1), wd=wd, epochs=15, callbacks=cbs)
+    learn.save_encoder(lm_encoder_name)
 
     return learn, data_lm
 
 
 def train_classification_model(data_dir, env, lm_data, sample_size, lm_encoder_name):
-
+    vocab = lm_data.train_ds.vocab if lm_data else None
     data_clas = TextClasDataBunch.from_csv(data_dir,
                                            train='train_clas',
                                            valid='valid_clas',
-                                           vocab=lm_data.train_ds.vocab,
-                                           chunksize=np.ceil(sample_size / 2) + 1)
+                                           vocab=vocab)
 
     learn = RNNLearner.classifier(
-        data_clas, drop_mult=0.5, clip=0.25, metrics=accuracy)
+        data_clas, drop_mult=0.5, clip=0.25, metrics=[accuracy])
 
     if env == 'floyd':
         cbs = [TensorboardLogger(learn, f"transfer-learning-{sample_size}"),
@@ -107,28 +91,30 @@ def train_classification_model(data_dir, env, lm_data, sample_size, lm_encoder_n
     else:
         cbs = list()
 
-    learn.metrics = [accuracy]
-    learn.load_encoder(lm_encoder_name)
+    try:
+        learn.load_encoder(lm_encoder_name)
+    except:
+        print(f"Couldn't find encoder {lm_encoder_name}")
 
     wd = 1e-7
-    learn.load_encoder('lm1_enc')
     learn.fit_one_cycle(max_lr=slice(1e-3 / 2, 1),
                         wd=wd, cyc_len=1, callbacks=cbs)
 
     return learn
 
 
-def train_lm_and_sentiment_classifier(exp_name, sample_size=1000, env='local', clean_run=True):
+def train_lm_and_sentiment_classifier(exp_name, sample_size=1000, env='local', clean_run=True, global_lm=True):
     data_dir = '_'.join([exp_name, str(sample_size)])
-    print(f'train.py data_directory {data_dir}  sample_size {sample_size}')
+    print(
+        f'train.py data_directory {data_dir} sample_size={sample_size} global_lm={global_lm} clean_run {clean_run} env {env}')
 
     sample_for_experiment(sample_size=sample_size,
                           dst=data_dir, env=env, clean_run=clean_run)
 
     print("Training Language Model...")
-    lm_learner, lm_data = train_language_model(data_dir, env, sample_size)
     lm_encoder_name = 'lm1_enc'
-    lm_learner.save_encoder(lm_encoder_name)
+    lm_learner, lm_data = train_language_model(
+        data_dir, env, sample_size, global_lm, lm_encoder_name)
 
     print("Training Sentiment Classifier...")
     sentiment_learner = train_classification_model(
